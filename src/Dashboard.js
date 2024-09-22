bd4a2aa5f35a793acd41eb20fb32def0afeb5e2d
Rodrigo <rodrigoriveramacias41@gmail.com>
2024-09-22 11:32:23 -0600

    Imagenes dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Dashboard.css';
+import Vector from './images/Vector.png';
+import boton from './images/boton.png'; // Importa la imagen para "Add Sensor"
+import usuario from './images/usuario.png'; // Importa la imagen para "Profile"
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -10,10 +13,23 @@ function Dashboard() {
       <div className="sidebar">
         <h2>SmartFarm</h2>
         <button className="summary-btn" onClick={() => navigate('/dashboard')}>
-          Summary
+          <div className="button-content">
+            <img src={Vector} alt="Icon" className="icon" />
+            Summary
+          </div>
         </button>
-        <button onClick={() => navigate('/add-sensor')}>Add Sensor</button>
-        <button onClick={() => navigate('/profile')}>Profile</button>
+        <button className="add-sensor-btn" onClick={() => navigate('/add-sensor')}>
+          <div className="button-content">
+            <img src={boton} alt="Add Sensor" className="icon" />
+            Add Sensor
+          </div>
+        </button>
+        <button className="profile-btn" onClick={() => navigate('/profile')}>
+          <div className="button-content">
+            <img src={usuario} alt="Profile" className="icon" />
+            Profile
+          </div>
+        </button>
       </div>
       <div className="content">
         <h1>Content</h1>
